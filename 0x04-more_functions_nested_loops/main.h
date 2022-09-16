e1e9b49087e146f753971036aafbe2cd2c1d165f
Ugo1000 <ugo.samuel.java@gmail.com>
2022-09-15 20:52:26 -0700

    print triangle

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,15 @@
-#ifndef _main_h_
-#define _main_h_
+#ifndef MAIN_H
+#define MAIN_H
 
+/**
+ * header file: main.h
+ */
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-int _putchar(char);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
